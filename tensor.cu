486e7aecb4a0f0da6333b44121e90d92ec13ffe4
RuanPetrus <xastroboyx11@gmail.com>
2025-04-17 10:18:05 -0300

    Moving kernels to kernels.cu

--- tensor.cu
+++ tensor.cu
@@ -1,205 +1,13 @@
 #include <assert.h>
 #include <random>
+#include "kernels.cu"
 
 #define TODO(message) do { fprintf(stderr, "%s:%d: TODO: %s\n", __FILE__, __LINE__, message); abort(); } while(0)
 #define ERROR(...) do { fprintf(stderr, __VA_ARGS__); abort(); } while(0)
-#define KEY(i3, i2, i1, d3, d2, d1) (d3*0 + i3*d2*d1 + i2*d1 + i1)
-#define SWAP(a, b, T) { T temp = a; a = b; b = temp; }
 
 #define THREAD_PER_BLOCK_X (1 << 4)
 #define THREAD_PER_BLOCK_Y (1 << 3)
 #define THREAD_PER_BLOCK_Z (1 << 3)
-
-/* === Binary Ops  === */
-__global__ void kernel_add(float *c, float * a, float *b,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] + 
-		b[KEY(ci3, ci2, ci1, cd3, cd2, cd1)];
-	}
-}
-
-__global__ void kernel_mul(float *c, float * a, float *b,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] *
-		b[KEY(ci3, ci2, ci1, cd3, cd2, cd1)];
-	}
-}
-
-__global__ void kernel_div(float *c, float * a, float *b,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] /
-		b[KEY(ci3, ci2, ci1, cd3, cd2, cd1)];
-	}
-}
-
-/* === Unary Ops  === */
-
-__global__ void kernel_exp(float *c, float * a,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		exp(a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)]);
-	}
-}
-
-__global__ void kernel_log(float *c, float * a,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		log(a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)]);
-	}
-}
-
-/* === re-shape Ops  === */
-
-__global__ void kernel_sum(float *c, float *a, 
-							int cd1, int cd2, int cd3, // c dimensions
-							int ad1, int ad2, int ad3) // a dimensions
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	int kn = ad3 / cd3;
-	int jn = ad2 / cd2;
-	int in = ad1 / cd1;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		float sum = 0;
-		for (int k = 0; k < kn; k++)
-		for (int j = 0; j < jn; j++)
-		for (int i = 0; i < in; i++) {
-			int ai3 = ci3 + k;
-			int ai2 = ci2 + j;
-			int ai1 = ci1 + i;
-			sum += a[KEY(ai3, ai2, ai1, ad3, ad2, ad1)];
-		}
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = sum;
-	}
-}
-
-__global__ void kernel_broadcast(float *c, float *a, 
-								 int cd1, int cd2, int cd3,
-								 int ad1, int ad2, int ad3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	int f1 = cd1 / ad1;
-	int f2 = cd2 / ad2;
-	int f3 = cd3 / ad3;
-
-	int ai3 = ci3 / f3;
-	int ai2 = ci2 / f2;
-	int ai1 = ci1 / f1;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		// c[id3][id2][id1] = a[ai3][ai2][ai1];
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = a[KEY(ai3, ai2, ai1, ad3, ad2, ad1)];
-	}
-}
-
-// op is the dimension that does not change
-__global__ void kernel_transpose(float *c, float *a, 
-								int cd1, int cd2, int cd3,
-								int op) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		int ai1 = ci1, ai2 = ci2, ai3 = ci3;
-		int ad1 = cd1, ad2 = cd2, ad3 = cd3;
-		if (op == 3) {
-			SWAP(ci1, ci2, int);
-			SWAP(cd1, cd2, int);
-		}
-		else if (op == 2) {
-			SWAP(ci1, ci3, int);
-			SWAP(cd1, cd3, int);
-		}
-		else if (op == 1) {
-			SWAP(ci2, ci3, int);
-			SWAP(cd2, cd3, int);
-		}
-		else {
-			// TODO: Error message
-		}
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = a[KEY(ai3, ai2, ai1, ad3, ad2, ad1)];
-	}
-}
-
-
-/* === Mat mul  === */
-__global__ void kernel_matmul(float *c, float *a, float *b, 
-							   int cd1, int cd2, int cd3,
-                               int ad1)
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	int ad3 = cd3, ad2 = cd2;
-	int bd3 = cd3, bd2 = ad1, bd1 = cd1;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		float sum = 0;
-		for (int k = 0; k < ad1; k++) {
-			int ai2 = ci2, ai1 = k;
-			int bi2 = k, bi1 = ci1;
-			sum += a[KEY(ci3, ai2, ai1, ad3, ad2, ad1)] * 
-				   b[KEY(ci3, bi2, bi1, bd3, bd2, bd1)];
-		}
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = sum;
-	}
-}
-
-/* === Scalar Ops  === */
-__global__ void kernel_mul_scalar(float *c, float * a, float b,
-						   int cd1, int cd2, int cd3) 
-{
-	int ci3 = blockIdx.z * blockDim.z + threadIdx.z;
-	int ci2 = blockIdx.y * blockDim.y + threadIdx.y;
-	int ci1 = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (ci3 < cd3 && ci2 < cd2 && ci1 < cd1) {
-		c[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] = 
-		a[KEY(ci3, ci2, ci1, cd3, cd2, cd1)] * b; 
-	}
-}
 
 static int TENSOR_GPU_LAST_SYNC = 0;
 
@@ -209,6 +17,11 @@ float random_normal_distribution_float()
 	static std::mt19937 random_generator{random_device()};
 	static std::normal_distribution random_normal_distribution{0.0f, 1.0f};
 	return random_normal_distribution(random_generator);
+}
+
+int int_ceil(int a, int b) 
+{
+	return (a + b-1) / b;
 }
 
 struct Tensor3 
@@ -301,11 +114,6 @@ void tensor3_show(Tensor3 t)
 		}
 		printf("]\n");
 	}
-}
-
-int int_ceil(int a, int b) 
-{
-	return (a + b-1) / b;
 }
 
 bool tensor3_same_shape(Tensor3 a, Tensor3 b)
@@ -427,6 +235,7 @@ Tensor3 tensor3_transpose(Tensor3 a, int dx, int dy)
 Tensor3 tensor3_view(Tensor3 a, int d1, int d2, int d3) 
 {
 	assert(d1*d2*d3 == a.d1*a.d2*a.d3);
+	sync(a);
 	Tensor3 c = tensor3_new(d1, d2, d3);
 
 	cudaMemcpy(c.data, a.data, d1*d2*d3*sizeof(float), cudaMemcpyDeviceToDevice);
